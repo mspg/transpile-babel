38dd73ad11de97a8b173cde5748fd885c42c0339
Jascha <jascha@jaeh.at>
2019-01-05 20:27:38 +0100

    update babel to 7, return errors instead of throwing

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
-const babel = require('babel-core')
+const babel = require('@babel/core')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const defaultConfig = {
-  sourceMap: isProd ? false : 'both',
-  presets: [
-    'minify',
-    'env',
-    'stage-0',
-    [
-      'env',
-      {
-        targets: {
-          browsers: ['last 2 versions', 'safari >= 7'],
-        },
-      },
-    ],
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      targets: '>0.25%, not dead',
+      // useBuiltIns: 'entry',
+      forceAllTransforms: true,
+      ignoreBrowserslistConfig: true,
+      modules: false,
+      debug: !isProd,
+    },
   ],
+]
+
+const plugins = ['@babel/plugin-transform-arrow-functions']
+if (isProd) {
+  plugins.push('minify-mangle-names')
+  plugins.push('minify-dead-code-elimination')
 }
 
-const BABEL = ({ buffer, name, config }) => {
+const defaultConfig = {
+  sourceMap: isProd ? false : 'both',
+  comments: !isProd,
+  presets,
+  plugins,
+}
+
+const BABEL = (args = {}) => {
   try {
+    const { buffer, name, config } = args
     if (!buffer) {
-      throw new Error('BABEL: expect first argument to include { buffer }')
+      throw new Error('BABEL expects args to include { buffer }')
+    }
+    if (!name) {
+      throw new Error('BABEL expects args to include { name }')
+    }
+    if (!config) {
+      throw new Error('BABEL expects args to include { config }')
     }
 
     config.basedir = config.HTML_DIR
-
-    const babelOptions = Object.assign({ filename: name }, config.BABEL, defaultConfig)
+    const babelOptions = { ...defaultConfig, ...config.BABEL, filename: name }
 
     const { code } = babel.transform(buffer, babelOptions)
     return code
   } catch (e) {
-    throw e
+    return e
   }
 }
 
--- test/spec.js
+++ test/spec.js
@@ -1,28 +1,32 @@
 const { is, tryCatch } = require('@magic/test')
-//
+
 const BABEL = require('../src/index.js')
 
 const config = {
-  CSS_DIR: __dirname,
   ENV: 'development',
 }
 
 const buffer = 'console.log("ohai")'
-const expect = r => r.indexOf('"use strict";console.log("ohai");') === 0
+const expect = r => r.includes('console.log("ohai");')
 
-const invalidBuffer = 'console.log(nonexistent.deep.object)'
+// const invalidBuffer = 'console.log(nonexistent.deep.object)'
 
 module.exports = [
   { fn: () => BABEL, expect: is.fn, info: 'BABEL is a function' },
-  { fn: BABEL({ buffer, config }), expect, info: 'BABEL can transpile' },
+  { fn: BABEL({ buffer, config, name: 'test' }), expect, info: 'BABEL can transpile' },
   {
-    fn: tryCatch(BABEL, { config }),
-    expect: is.error,
-    info: 'Calling BABEL without a buffer errors',
+    fn: BABEL({ config, name: 'test' }),
+    expect: b => is.error(b) && b.toString().includes('{ buffer }'),
+    info: 'Calling BABEL without a buffer returns correct error',
   },
   {
-    fn: tryCatch(BABEL),
-    expect: is.error,
-    info: 'Calling BABEL without config errors',
+    fn: BABEL({ buffer: 'test', name: 'test' }),
+    expect: b => is.error(b) && b.toString().includes('{ config }'),
+    info: 'Calling BABEL without config returns correct error',
+  },
+  {
+    fn: BABEL({ buffer: 'test', config }),
+    expect: b => is.error(b) && b.toString().includes('{ name }'),
+    info: 'Calling BABEL without name returns correct error',
   },
 ]
